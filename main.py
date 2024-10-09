0b86d0af089280c0e536019907634c37ab22fc68
Gwynn <nikolaj.schoeppler@gmail.com>
2024-10-09 21:08:40 +0200

    added player visual logics

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pygame
 from constants import *
+from player import *
+
+py_clock = pygame.time.Clock()
+dt = 0
+player_pos_x = SCREEN_WIDTH/2
+player_pos_y = SCREEN_HEIGHT/2
 
 def main():
     print("Starting asteroids!")
@@ -7,12 +13,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    py_player = Player(player_pos_x,player_pos_y)
+    
     while True:
         screen.fill(color=(0,0,0))
+        py_player.draw(screen)
         pygame.display.flip()
+        
+        # Close function
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        dt = py_clock.tick(60) / 1000
 
 
 
